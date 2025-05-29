44c5dfb53c2fd152fc09f484735f78467f3740f5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-05-29 13:23:45 -0400

    added dynamic functionality for tabs. when clicked on that button it shows that page.

--- tab-from-component/src/App.js
+++ tab-from-component/src/App.js
@@ -1,10 +1,41 @@
-
-import './App.css';
+import "./App.css";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
+import Interest from "./components/Interest";
+import { useState } from "react";
 
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const activeTabs = [
+    {
+      name: "profile",
+      component: Profile,
+    },
+    {
+      name: "settings",
+      component: Settings,
+    },
+    {
+      name: "Interests",
+      component: Interest,
+    },
+  ];
+
+  const ActiveTabComponent = activeTabs[tabIndex].component;
+
   return (
     <div className="App">
-     tab form component
+      {activeTabs.map((x, i) => {
+        return (
+          <>
+            <button onClick={() => setTabIndex(i)}>{x.name}</button>
+          </>
+        );
+      })}
+      <div>
+        <ActiveTabComponent />
+      </div>
     </div>
   );
 }
