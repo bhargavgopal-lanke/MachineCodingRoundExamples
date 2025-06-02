79ab031861405ef28f366fe25ee834737bb84595
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-02 14:46:13 -0400

    code optimised in app.js file

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
+import Pagination from "./components/Pagination";
+import { fetchData } from "./Apis/FetchData";
 
 const PAGE_SIZE = 10;
 
@@ -8,38 +10,22 @@ function App() {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrenpage] = useState(0);
 
-  // make an API call
-  const fetchData = async () => {
+  const getData = async () => {
     try {
-      const data = await fetch("https://dummyjson.com/products?limit=200");
-      const json = await data.json();
-      console.log(json?.products);
-      setProducts(json?.products);
+      const data = await fetchData();
+      setProducts(data);
     } catch (err) {
       console.log(err.message);
     }
   };
-
   useEffect(() => {
-    fetchData();
+    getData();
   }, []);
 
   const totalProducts = products.length;
   const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
-
   const start = currentPage * PAGE_SIZE;
   const end = start + PAGE_SIZE;
-
-  const handleChange = (i) => {
-    setCurrenpage(i);
-  };
-
-  const goToNextpage = () => {
-    setCurrenpage((prev) => prev + 1);
-  };
-  const goToPrevpage = () => {
-    setCurrenpage((prev) => prev - 1);
-  };
 
   return (
     <div className="App">
@@ -47,31 +33,11 @@ function App() {
         <h1>No Products Found</h1>
       ) : (
         <>
-          <div className="pagination">
-            <button onClick={goToPrevpage} disabled={currentPage === 0}>
-              ⬅️
-            </button>
-            {[...Array(noOfPages).keys()].map((x, i) => {
-              return (
-                <div key={x}>
-                  <button
-                    className={
-                      "pagination-numbers" + x === currentPage && " active"
-                    }
-                    onClick={() => handleChange(x)}
-                  >
-                    {x}
-                  </button>
-                </div>
-              );
-            })}
-            <button
-              onClick={goToNextpage}
-              disabled={currentPage === noOfPages - 1}
-            >
-              ➡️
-            </button>
-          </div>
+          <Pagination
+            noOfPages={noOfPages}
+            setCurrenpage={setCurrenpage}
+            currentPage={currentPage}
+          />
           <div className="prodcuts-grid">
             {products.slice(start, end).map((x) => {
               return <ProductCard key={x.id} data={x} />;
