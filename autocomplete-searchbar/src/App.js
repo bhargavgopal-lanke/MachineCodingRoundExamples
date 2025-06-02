8ddba99d3dd679bdd745ea1720e3dcdb418f57a8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-02 15:54:16 -0400

    fetching the data from youtube serahc api v3.

--- autocomplete-searchbar/src/App.js
+++ autocomplete-searchbar/src/App.js
@@ -1,10 +1,36 @@
-
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { fetchSearchList } from "./APis/searchApi";
 
 function App() {
+  const [inputValue, setInputvalue] = useState("");
+  const [searchApiData, setSearchApiData] = useState([]);
+  const getSearchData = async () => {
+    try {
+      const data = await fetchSearchList();
+      setSearchApiData(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(searchApiData);
+
+  useEffect(() => {
+    getSearchData();
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>Autocomplete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="form-control"
+          value={inputValue}
+          onChange={(e) => setInputvalue(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
