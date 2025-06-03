47ed2903fe0a03b692cdf329631825ec98bdcebe
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-03 18:41:47 -0400

    progress bar is completed.

--- progressbar/src/components/progressbar.jsx
+++ progressbar/src/components/progressbar.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Progressbar = ({ progress }) => {
+  const [animatedProgress, setAnimatedProgress] = useState(0);
+
+  useEffect(() => {
+    const progressTimer = setTimeout(() =>  setAnimatedProgress(progress), 100);
+    return () => {
+      clearTimeout(progressTimer);
+    };
+  });
+
   return (
     <div>
       <div className="progress-bar-main">
         <div
           className="inner"
           style={{
-            width: `${progress}%`,
-            color: progress < 5 ? "black" : "white",
+            // width: `${progress}%`,
+            transform: `translateX(${animatedProgress - 100}%)`,
+            color: animatedProgress < 5 ? "black" : "white",
           }}
           role="progressbar"
           aria-valuenow={progress}
