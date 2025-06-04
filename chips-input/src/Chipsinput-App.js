98ab6a7b77c63fa8ece105a1b199a69c49ddf3c7
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-04 19:02:26 -0400

    Boiler plate removed.

---
+++ chips-input/src/Chipsinput-App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChipsInputMain from "./components/ChipsInputMain";
+
+function App() {
+  return (
+    <div className="App">
+      <ChipsInputMain />
+    </div>
+  );
+}
+
+export default App;
