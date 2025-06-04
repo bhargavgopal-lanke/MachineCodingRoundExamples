7345cb84c05f984c82e04e2a0c66db522730ad15
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-04 18:48:14 -0400

    Nested checkboxes code.

--- nestedcheckboxes/src/App.js
+++ nestedcheckboxes/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Checkbox from "./components/Checkbox";
+import { CHECKBOXESDATA } from "./data/data";
 
 function App() {
+  const [checkbox, setCheckbox] = useState({});
   return (
     <div className="App">
       <h1>Nested Checkboxes</h1>
-      
+      <Checkbox
+        data={CHECKBOXESDATA}
+        checkbox={checkbox}
+        setCheckbox={setCheckbox}
+      />
     </div>
   );
 }
---
+++ nestedcheckboxes/src/data/data.js
@@ -0,0 +1,54 @@
+export const CHECKBOXESDATA = [
+  {
+    id: 1,
+    name: "Fruits",
+    children: [
+      {
+        id: 2,
+        name: "Citrus",
+        children: [
+          {
+            id: 3,
+            name: "lemon",
+          },
+          {
+            id: 4,
+            name: "Orange",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "Berries",
+        children: [
+          {
+            id: 6,
+            name: "Starwberry",
+          },
+          {
+            id: 7,
+            name: "Blueberry",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Tropical",
+    children: [
+      {
+        id: 9,
+        name: "Mango",
+      },
+      {
+        id: 10,
+        name: "Banana",
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "Apple",
+  },
+];
