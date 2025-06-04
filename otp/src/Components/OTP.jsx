f177a6081f18171138c32a922d75705431d91525
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-04 15:49:03 -0400

    added comments for complicated code.

--- otp/src/Components/OTP.jsx
+++ otp/src/Components/OTP.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { OTP_DIGIT_COUNT } from "../utils/util";
 
 const OTP = () => {
+  const refArr = useRef([]);
   const [inputArray, setInputArray] = useState(
     new Array(OTP_DIGIT_COUNT).fill("")
   );
+
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  }, []);
 
   const handleChange = (e, i) => {
     const value = e.target.value;
@@ -12,12 +17,20 @@ const OTP = () => {
     if (isNaN(value)) {
       return;
     }
+    const newValue = value.trim();
     const newArray = [...inputArray]; // copied the array into a new variable
-    newArray[i] = value; // adding the value for that index position inside the array
+    newArray[i] = newValue.slice(-1); // adding the value for that index position inside the array
     setInputArray(newArray); // updating the inputArray variable with setter function
+    newValue && refArr.current[i + 1]?.focus();
   };
 
-  console.log(inputArray);
+  const handleOnkeyDown = (e, i) => {
+    // if my input box is empty then only i want to move to previous one.
+    // "!e.target.value" i only want to move to previous one if the input value is empty.
+    if (!e.target.value && e.key === "Backspace") {
+      refArr.current[i - 1]?.focus();
+    }
+  };
 
   return (
     <div>
@@ -29,8 +42,10 @@ const OTP = () => {
               key={i}
               className="otp-input"
               type="text"
+              ref={(input) => (refArr.current[i] = input)}
               value={inputArray[i]}
               onChange={(e) => handleChange(e, i)}
+              onKeyDown={(e) => handleOnkeyDown(e, i)}
             />
           );
         })}
