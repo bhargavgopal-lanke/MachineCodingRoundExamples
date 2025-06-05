5f33b85e55bf919447a96e01ce43a11998ae2504
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-06-05 18:21:47 -0400

    If no items was there add a string to hanlde the error.

--- accordian/src/components/Accordian.jsx
+++ accordian/src/components/Accordian.jsx
@@ -7,23 +7,27 @@ const Accordian = ({ items }) => {
     setIndex(openIndex === index ? null : index);
   };
 
-
   return (
     <div>
-      {items.map((item, i) => {
-        return (
-          <div className="accordian" key={i}>
-            <h1 className="parent" onClick={() => hanldeClick(i)}>
-              {item.title}
-            </h1>
-            {openIndex === i && (
-              <div className={`child`}>
-                <p>{item.content}</p>
-              </div>
-            )}
-          </div>
-        );
-      })}
+      {items.length === 0 ? (
+        <h1>No items found</h1>
+      ) : (
+        items.map((item, i) => {
+          return (
+            <div className="accordian" key={i}>
+              <h1 className="parent" onClick={() => hanldeClick(i)}>
+                {item.title}
+                <span className="icon">{openIndex === i ? "⬆️" : "⬇️"}</span>
+              </h1>
+              {openIndex === i && (
+                <div className={`child`}>
+                  <p>{item.content}</p>
+                </div>
+              )}
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
